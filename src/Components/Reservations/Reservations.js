f01fd75e1c6aceedbaaff433fa3133a157c0adee
Angela Battillo <angela.battillo@gmail.com>
2021-05-26 10:43:05 -0600

    Add destructured data from view of api call data file

--- src/Components/Reservations/Reservations.js
+++ src/Components/Reservations/Reservations.js
@@ -6,7 +6,16 @@ const Reservations = ({ reservations }) => {
 
   const reservationCards = reservations.map(reservation => {
     return(
-
+      <div>
+        <Card
+          id={reservation.id}
+          name={reservation.name}
+          date={reservation.date}
+          time={reservation.time}
+          count={reservation.number}
+          key={reservation.id}
+        />
+      </div>
     )
   })
 
