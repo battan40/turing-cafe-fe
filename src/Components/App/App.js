6b4bd9d68ead49ab0da3aaab2ccc31586575d496
Angela Battillo <angela.battillo@gmail.com>
2021-05-26 10:41:21 -0600

    Show rendering of reservations in main app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Reservations from '../Reservations/Reservations'
 import {fetchReservations } from '../../Utils/ApiCalls'
 import './App.css';
 
@@ -19,13 +20,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-
-        </div>
+        <h1 className='app-title'>Turing Cafe Reservations</h1>  
+          <Reservations className='resy-container' reservations={this.state.reservations}/>
       </div>
     )
   }
