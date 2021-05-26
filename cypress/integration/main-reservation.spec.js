41f6396a0e3504cc59ee031eefe2382268904bc5
Angela Battillo <angela.battillo@gmail.com>
2021-05-26 12:02:10 -0600

    Test for submit button clicking

--- cypress/integration/main-reservation.spec.js
+++ cypress/integration/main-reservation.spec.js
@@ -9,13 +9,16 @@ describe('Show main view of Turing Reservation App', () => {
     cy.contains('h1', 'Turing Cafe Reservations')
   })
 
-  it ('Should display form input lines for name, number and button to submit', () => {
+  it('Should display form input lines for name, number and button to submit', () => {
     cy.get('form').find('input').should('be.visible')
       .get('form input[type=text]').should('be.visible')
       .get('form input[type=number]').should('be.visible')
       .get('form').find('button').should('be.visible')
   })
 
+  it('Should be able to click and make new reservation', () => {
+    cy.get('form').click()
+  })
 
 
 })
