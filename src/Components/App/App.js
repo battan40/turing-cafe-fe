e090f0ca7950dbdceae2d54cded891f89499e6f8
Angela Battillo <angela.battillo@gmail.com>
2021-05-26 10:11:06 -0600

    Create state skeleton

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import {fetchReservations } from '../../Utils/ApiCalls'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+      error: ''
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,7 +21,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
