17112b4d62645255ba9c4dc46c250e2a711b8e93
Angela Battillo <angela.battillo@gmail.com>
2021-05-26 12:12:32 -0600

    Add skeleton of post request

--- src/Utils/ApiCalls.js
+++ src/Utils/ApiCalls.js
@@ -2,5 +2,22 @@ export const fetchReservations = () => {
   return fetch('http://localhost:3001/api/v1/reservations')
            .then(response => response.json())
            .then(data => data)
-           .catch(err => err.message)
+           .catch(err => "Sorry we're having trouble setting that reservation, please call our cafe directly")
+}
+
+export const postNewReservation = (rezoData) => {
+  const init = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(rezoData),
+  }
+  return fetch('http://localhost:3001/api/v1/reservations', init)
+           .then(response => {
+             if(response.ok) {
+               return response.json()
+             }
+           })
+           .catch(err => "Please try again later, it looks we're having trouble with our server today")
 }
