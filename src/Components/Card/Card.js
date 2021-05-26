76b7c7c12b9e94178300b1e623a21bbe1392777f
Angela Battillo <angela.battillo@gmail.com>
2021-05-26 10:44:49 -0600

    Bring in Card view as desired

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Card.css'
 
-const Card = (
+const Card = ({ name, date, time, count }) => {
+  return(
+    <article className='card'>
+      <h3>{name}</h3>
+      <h4>{date}</h4>
+      <h4>{time}</h4>
+      <h4>{count}</h4>
+    </article>
+  )
 }
 
 export default Card;
